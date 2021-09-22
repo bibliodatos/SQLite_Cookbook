f6e0ab101e1e3eb10d0fb25eec8f193885a4bf2c
Mike <Mike@cg100840.home>
2021-09-22 16:30:08 -0700

    add emp_bonus table

--- create_tables.sql
+++ create_tables.sql
@@ -1,5 +1,16 @@
 DROP TABLE IF EXISTS emp;
 
+CREATE TABLE emp_bonus (
+  empno INTEGER NOT NULL,
+  received TEXT default NULL,
+  type INTEGER NOT NULL
+);
+
+INSERT INTO emp_bonus VALUES (7369, '14-MAR-2005', 1);
+INSERT INTO emp_bonus VALUES (7900, '14-MAR-2005', 2);
+INSERT INTO emp_bonus VALUES (7788, '14-MAR-2005', 3);
+
+
 CREATE TABLE emp (
   empno INTEGER NOT NULL,
   ename TEXT default NULL,
