ffda1f6b9c5d0156fed2f30b3b663935650a1f8e
Mike <Mike@cg100840.home>
2021-09-21 20:02:43 -0700

    1.6.sql Referencing aliased column in WHERE clause

---
+++ chapter_1/1.6.sql
@@ -0,0 +1,10 @@
+-- Referencing an aliased column in WHERE clause
+
+SELECT *
+  FROM (
+    SELECT sal AS salary, comm AS commission
+    FROM emp
+  ) AS x
+WHERE salary < 5000;
+
+-- no differences from MySQL
