31454a9da1b02a55efd4d5c0098a71ee1ce67dd7
Mike <Mike@cg100840.home>
2021-09-22 11:43:38 -0700

    Returning results in a specified order

---
+++ chapter_2/2.1.sql
@@ -0,0 +1,21 @@
+-- Query results in a specified order
+
+-- Order by smallest salary to largest salary
+SELECT ename, job, sal
+  FROM emp
+  WHERE deptno = 10
+  ORDER BY sal ASC;
+
+-- Order by largest salary to smallest salary
+SELECT ename, job, sal
+  FROM emp
+  WHERE deptno = 10
+  ORDER BY sal DESC;
+
+-- Order by largest value in third column to smallest
+SELECT ename, job, sal
+  FROM emp
+  WHERE deptno = 10
+  ORDER BY 3 DESC;
+
+-- no differences from MySQL
