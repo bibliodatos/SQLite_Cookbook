312496b954a59486c2916efd87de2dc83097d445
Mike <Mike@cg100840.home>
2021-09-22 15:54:51 -0700

    Combining related row with a join

---
+++ chapter_3/3.2.sql
@@ -0,0 +1,15 @@
+-- Example 3.2 Combining related rows
+
+-- Join emp table to dept table on deptno field. Implicit join.
+SELECT e.ename, d.loc
+  FROM emp AS e, dept AS d
+WHERE e.deptno = d.deptno
+  AND e.deptno = 10
+
+-- Join emp table to dept table on deptno field. Explicit inner join.
+SELECT e.ename, d.loc
+  FROM emp AS e
+  INNER JOIN dept AS d ON (e.deptno = d.deptno)
+WHERE e.deptno = 10
+
+-- No difference between SQLite and MySQL
