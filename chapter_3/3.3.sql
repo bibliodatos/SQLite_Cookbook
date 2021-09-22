5e035c4caf5904a40cc2dc1f59a69f5ea76b6bda
Mike <Mike@cg100840.home>
2021-09-22 16:05:16 -0700

    Finding rows in common between two tables

---
+++ chapter_3/3.3.sql
@@ -0,0 +1,15 @@
+-- Example 3.2 Finding rows in common between two tables
+
+-- Create view 
+Create view V
+AS
+SELECT ename, job, sal
+  FROM emp
+  WHERE job = 'CLERK';
+
+-- Join emp table to V view with explicit join
+SELECT e.empno, e.ename, e.job, e.sal, e.deptno
+  FROM emp AS e
+  JOIN V on (e.ename = v.ename AND e.job = v.job AND e.sal = v.sal);
+
+-- No difference between SQLite and MySQL
