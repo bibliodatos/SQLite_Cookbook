2fb64b74a9f75b8e8c01f95de8558d455f4a5bda
Mike <Mike@cg100840.home>
2021-09-22 13:36:45 -0700

    Sorting on a data-dependent key

---
+++ chapter_2/2.6.sql
@@ -0,0 +1,8 @@
+-- Example 2.6 Sorting on a data-dependent key
+
+-- Sort by commission if job is 'SALESPERSON' otherwise sort by salary
+SELECT ename, sal, job, comm
+  FROM emp
+ORDER BY CASE WHEN job = 'SALESPERSON' THEN comm ELSE sal END
+
+-- No differences from SQLite to MySQL
