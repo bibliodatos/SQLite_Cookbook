16e6c56d5885aeae44f6ccfbe6180f7705d5cece
Mike <Mike@cg100840.home>
2021-09-21 20:51:24 -0700

    1.13.sql Searching for patterns

---
+++ chapter_1/1.13.sql
@@ -0,0 +1,13 @@
+-- Searching for patterns
+SELECT ename, job
+  FROM emp
+WHERE deptno IN (10,20)
+  AND (ename LIKE '%I%' OR job LIKE '%ER')
+;
+
+/* Employees in departments 10 and 20 return only those that have either
+an "I" somewhere in their name or job title ending with "ER"
+*/
+
+
+-- no differences from MySQL
