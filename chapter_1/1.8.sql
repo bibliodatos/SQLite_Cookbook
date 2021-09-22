b12c26c19f3f5b93452b69a14e72c7fc366f873e
Mike <Mike@cg100840.home>
2021-09-21 20:16:25 -0700

    1.8.sql Conditional logic in SELECT

---
+++ chapter_1/1.8.sql
@@ -0,0 +1,10 @@
+-- Conditional logic in a SELECT
+
+SELECT ename, sal,
+    CASE WHEN sal <= 2000 THEN 'UNDERPAID'
+         WHEN sal >= 4000 THEN 'OVERPAID'
+         ELSE 'OK'
+    END AS status
+FROM emp;
+
+-- no differences from MySQL
