29af0fafe19fa80158a3a0ff531a3765f60db78c
Mike <Mike@cg100840.home>
2021-09-22 11:48:49 -0700

    sorting by multiple fields

---
+++ chapter_2/2.2.sql
@@ -0,0 +1,8 @@
+-- Query results sorted by multiple fields
+
+-- Order by department number first and then salary within the department
+SELECT ename, deptno, sal, ename, job
+  FROM emp
+  ORDER BY deptno, sal DESC;
+
+-- no differences from SQLite to MySQL
