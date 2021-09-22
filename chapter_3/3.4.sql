b167962727675a9dddf11fe739150460ddf87cdc
Mike <Mike@cg100840.home>
2021-09-22 16:14:33 -0700

    Selecting values from one table that don't exist in another

---
+++ chapter_3/3.4.sql
@@ -0,0 +1,15 @@
+-- Example 3.4 Selecting values from one table that don't exist in another
+
+-- SQLite has EXCEPT operator
+SELECT deptno FROM dept
+EXCEPT
+SELECT deptno FROM emp;
+
+
+/*
+ MySQL does not have EXCEPT operator.
+This also works in SQLite
+*/
+
+SELECT deptno FROM dept
+WHERE deptno NOT IN (SELECT deptno FROM emp);
