e45175da15dda80b07cf62a5c169628a3ccd439d
Mike Maynard <mayste@gmail.com>
2021-09-28 19:55:48 -0700

    4.3.sql

---
+++ chapter_4/4.3.sql
@@ -0,0 +1,12 @@
+-- Example 4.2 Overriding default values using NULL
+
+SAVEPOINT "RESTOREPOINT";
+
+CREATE TABLE D (id integer default 0, foo varchar(10));
+
+-- Use NULL instead of default
+INSERT INTO D(id, foo) VALUES(NULL, 'Brighten');
+
+ROLLBACK TO SAVEPOINT "RESTOREPOINT";
+
+-- No difference between SQLite and MySQL
