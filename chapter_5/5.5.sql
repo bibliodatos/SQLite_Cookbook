2e9556591fe5f18980d738ddbd223df60dc90679
Mike Maynard <mayste@gmail.com>
2021-09-29 19:42:09 -0700

    5.5

---
+++ chapter_5/5.5.sql
@@ -0,0 +1,19 @@
+-- Example 5.2 Listing indexed columns for a table
+
+CREATE INDEX empno_idx ON emp(empno);
+
+/*
+  You can't select specific column indexes in SQLite but you can
+  query for all the index names and try to pattern match the sql
+*/
+SELECT name AS index_name
+ FROM "main".sqlite_master
+ WHERE type = 'index'
+ AND  tbl_name = 'emp'
+ AND  sql LIKE '%empno%';
+
+
+/*
+  MySQL solution
+*/
+SHOW INDEX FROM emp;
