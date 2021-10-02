e62c479c8d7a91a787a62d10c725ff511e350ec6
Mike Maynard <mayste@gmail.com>
2021-10-01 18:54:52 -0700

    Parsing an IP Address

--- chapter_6/6.15.sql
+++ chapter_6/6.15.sql
@@ -1,33 +1,19 @@
--- Example 6.14 Extracting the nth delimited substring
+-- Example 6.15 Parsing an IP Address
 
-CREATE VIEW V AS
-  SELECT 'mo,larry,curly' AS csv
-  UNION ALL
-  SELECT 'tina,gina,juanita,regina,leena' AS csv;
 
 /*
-  SQLite solution uses a common table expression (CTE) as there is no
-  string function provided in SQLite that parses a string into parts.
-  Thank you to user1461607 on stackoverflow for the CTE example...
-    https://stackoverflow.com/questions/24258878/how-to-split-comma-separated-value-in-sqlite
-  As of yet I have not been able to figure out how to work with querying mulitple
-  rows from the view.  Below is an example with the string hard coded in the CTE.
+  Nothing in SQLite lets us split strings by a delimiter so we
+  have to get creative with making our delimiter very wide so
+  we can count on our number parts being in certain ranges.
 */
-WITH split(word, str) AS (
-	SELECT '', 'tina,gina,juanita,regina,leena,'
-    UNION ALL
-	SELECT
-		substr(str, 0, instr(str, ',')),
-		substr(str, instr(str, ',') +1)
-    FROM split WHERE str!=''
-  )
-SELECT word FROM split
-WHERE word != ''
-ORDER BY LENGTH(str) DESC
-LIMIT 1 OFFSET 1;
-
+SELECT REPLACE(SUBSTR(addr,1, 3), '_', '') AS a,
+	     REPLACE(SUBSTR(addr,13, 19), '_', '') AS b ,
+	     REPLACE(SUBSTR(addr,36, 19), '_', '') AS c,
+       REPLACE(SUBSTR(addr,55, 19), '_', '') AS d
+FROM
+  (SELECT REPLACE('92.111.0.2','.', '____________________') AS addr )
 
 /*
-MySQL solution has the SUBSTRING_INDEX function which makes this relatively
-simple to accomplish
+MySQL has the SUBSTRING_INDEX function which makes this relatively
+simple to accomplish.
 */
