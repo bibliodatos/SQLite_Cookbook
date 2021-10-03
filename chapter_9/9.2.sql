c4b6ba1683e6cca1b3965385c3fd196583f26b3e
Mike Maynard <mayste@gmail.com>
2021-10-02 21:33:04 -0700

    Determining the number of days in a year

--- chapter_9/9.2.sql
+++ chapter_9/9.2.sql
@@ -4,15 +4,14 @@
 	Use SQLite to calculate the number of days in the current year
 */
 SELECT
-		CAST(JULIANDAY(STRFTIME('%Y', 'NOW') ||'-12-31')
-		- JULIANDAY(STRFTIME('%Y', 'NOW')||'-01-01')
-		AS INTEGER) + 1 as days;
+  CAST(JULIANDAY(STRFTIME('%Y', 'NOW') ||'-12-31')
+  - JULIANDAY(STRFTIME('%Y', 'NOW')||'-01-01')
+  AS INTEGER) + 1 as days;
 
 -- Show a leap year exampl
 SELECT
-		CAST(JULIANDAY('2020-12-31')
-		- JULIANDAY('2020-01-01')
-		AS INTEGER) + 1 as days;
+  CAST(JULIANDAY('2020-12-31') - JULIANDAY('2020-01-01')
+  AS INTEGER) + 1 as days;
 
 /*
   We could do the same thing in MySQL but use CURRENT_DATE instead of NOW.
