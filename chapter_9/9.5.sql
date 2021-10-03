ec8d23e68bd078714fb349e3a6fd806ba0bf11be
Mike Maynard <mayste@gmail.com>
2021-10-02 22:37:01 -0700

    Determining all dates for a particular weekday throught a year

--- chapter_9/9.5.sql
+++ chapter_9/9.5.sql
@@ -1,9 +1,9 @@
--- Example 9.4 Determining all dates for a particular weekday throught a year
+-- Example 9.5 Determining all dates for a particular weekday throught a year
 
 /*
-	In SQLite find all the Fridays in the current year.
-	We utilize our dummy V500 view to iterate and generate a year's worth of dates.
-	From there it is simple to filter out only the Fridays.
+  In SQLite find all the Fridays in the current year.
+  We utilize our dummy V500 view to iterate and generate a year's worth of dates.
+  From there it is simple to filter out only the Fridays.
 */
 SELECT DATE('NOW','start of year', '+'||CAST(pos - 1 AS TEXT)||' day' ) as day
 FROM V500
@@ -14,5 +14,5 @@ LIMIT 366
 
 /*
   The example in SQL Cookbook for MySQL uses a recursive CTE. I like our simpler
-	solution shown above.
+  solution shown above.
 */
