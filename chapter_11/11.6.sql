dd10d0e75bcb04a465ac3435d2f1b4a6c4e5d8e4
Mike Maynard <mayste@gmail.com>
2021-10-16 18:41:19 -0700

    Finding records with the highest and lowest values

---
+++ chapter_11/11.6.sql
@@ -0,0 +1,16 @@
+-- Example 11.5 SELECTing the top N records
+
+
+/*
+  We want to find the employees with the top 5 salaries and we want to include
+  all ties in the result. Use DENSE_RANK window function.
+*/
+SELECT ename, sal
+FROM (
+  SELECT ename, sal, DENSE_RANK() OVER (ORDER BY sal DESC) AS dr
+  FROM emp
+) AS x
+WHERE dr <= 5;
+
+
+-- No difference between SQLite and MySQL
