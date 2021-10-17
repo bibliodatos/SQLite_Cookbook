89f67dd2fdd2a619a4bf4c78dbea297714010a52
Mike Maynard <mayste@gmail.com>
2021-10-17 13:51:24 -0700

    Reverse pivoting a result set into one column

---
+++ chapter_12/12.4.sql
@@ -0,0 +1,36 @@
+-- Example 12.4 Reverse pivoting a result set into one column
+
+/*
+  Return all the columns from a query in just one column. Return employee name,
+  job and salary of all employees in department 10.
+*/
+WITH four_rows (id)
+AS
+(
+	SELECT 1
+	UNION ALL
+	SELECT id + 1 FROM four_rows WHERE id < 4
+)
+,
+x_tab (ename, job, sal, rn)
+AS
+(
+	SELECT e.ename, e.job, e.sal,
+		ROW_NUMBER() OVER (PARTITION BY e.empno ORDER BY e.empno)
+	FROM emp AS e
+	JOIN four_rows on 1=1
+	WHERE e.deptno = 10
+)
+
+SELECT
+ CASE rn
+	WHEN 1 THEN ename
+	WHEN 2 THEN job
+	WHEN 3 THEN CAST(sal AS CHAR(4))
+	END AS emps
+FROM x_tab
+
+/*
+  No difference between SQLite and MySQL. We could CAST to TEXT instead of
+  CHAR(4) but SQLite accepts CHAR(4).
+*/
