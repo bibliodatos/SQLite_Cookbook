ee2c7f2ba9b2ba2cc0e53f5c3750d90c7637e9b7
Mike Maynard <mayste@gmail.com>
2021-12-26 13:43:05 -0800

    2.5

--- chapter_2/2.5.sql
+++ chapter_2/2.5.sql
@@ -2,17 +2,17 @@
   Query 2.5 - Dealing with NULLs when sorting
 */
 
--- 2.4.a SQLite NULLS sort first by default
+-- 2.5.a SQLite NULLS sort first by default
 SELECT ename, sal, comm
   FROM emp
   ORDER BY 3
 
--- 2.4.b Make NULLS sort last using DESC
+-- 2.5.b Make NULLS sort last using DESC
 SELECT ename, sal, comm
   FROM emp
   ORDER BY 3 DESC
 
--- 2.4.c NON-NULL commission sorted ascending and all NULLS last
+-- 2.5.c NON-NULL commission sorted ascending and all NULLS last
 SELECT ename, sal, comm
   FROM  (
     SELECT ename, sal, comm,
@@ -21,7 +21,7 @@ SELECT ename, sal, comm
   ) x
 ORDER BY is_null DESC, comm ASC
 
--- 2.4.d NON-NULL commission sorted descending and all NULLS last
+-- 2.5.d NON-NULL commission sorted descending and all NULLS last
 SELECT ename, sal, comm
   FROM  (
     SELECT ename, sal, comm,
@@ -30,7 +30,7 @@ SELECT ename, sal, comm
   ) x
 ORDER BY is_null DESC, comm DESC
 
--- 2.4.e NON-NULL commission sorted ascending and all NULLS first
+-- 2.5.e NON-NULL commission sorted ascending and all NULLS first
 SELECT ename, sal, comm
   FROM  (
     SELECT ename, sal, comm,
@@ -39,7 +39,7 @@ SELECT ename, sal, comm
   ) x
 ORDER BY is_null ASC, comm ASC
 
--- 2.4.f NON-NULL commission sorted descending and all NULLS first
+-- 2.5.f NON-NULL commission sorted descending and all NULLS first
 SELECT ename, sal, comm
   FROM  (
     SELECT ename, sal, comm,
